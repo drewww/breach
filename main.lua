c14f3bf0e8a3ab19db798c3f93e7eccb0e5cee33
Drew Harry <drew.harry@gmail.com>
2025-11-25 21:43:10 -0800

    Switched to CP437 fully, for now.

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ prism.defaultCell = prism.cells.Floor
 love.graphics.setDefaultFilter("nearest", "nearest")
 
 local macroAtlas = spectrum.SpriteAtlas.fromASCIIGrid("display/spritesheets/cp437_32x32.png", 32, 32)
-local microAtlas = spectrum.SpriteAtlas.fromASCIIGrid("display/spritesheets/wanderlust_16x16.png", 16, 16)
+local microAtlas = spectrum.SpriteAtlas.fromASCIIGrid("display/spritesheets/cp437_16x16.png", 16, 16)
 
 local display = spectrum.Display(SCREEN_WIDTH, SCREEN_HEIGHT, macroAtlas, prism.Vector2(32, 32))
 local overlayDisplay = spectrum.Display(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, microAtlas, prism.Vector2(16, 16))
