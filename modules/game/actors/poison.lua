8c902d7f0ef0c67739091945f34e204595943832
Drew Harry <drew.harry@gmail.com>
2025-11-26 16:55:30 -0800

    Tweaking poison behaviors, visualization, overlaps, and removing an unnecessary controller.

--- modules/game/actors/poison.lua
+++ modules/game/actors/poison.lua
@@ -1,9 +1,8 @@
 prism.registerActor("Poison", function(volume)
    return prism.Actor.fromComponents {
       prism.components.Name("Poison"),
-      prism.components.Drawable { index = 177, color = prism.Color4.WHITE, background = prism.Color4.LIME },
+      prism.components.Drawable { index = 177, color = prism.Color4.WHITE, background = prism.Color4.LIME, layer = 10 },
       prism.components.Position(),
-      prism.components.WaitController(),
       prism.components.Gas("poison", volume or 50)
    }
 end)
