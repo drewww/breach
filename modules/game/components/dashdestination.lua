7e6e11123efa9e2cfd18cf30206aac836d81b7ef
Drew Harry <drew.harry@gmail.com>
2025-12-03 15:43:25 -0800

    Removing unused component.

--- modules/game/components/dashdestination.lua
+++
@@ -1,8 +0,0 @@
---- @class DashDestination : Component
-local DashDestination = prism.Component:extend("DashDestination")
-DashDestination.name = "DashDestination"
-
-function DashDestination:__new()
-end
-
-return DashDestination
