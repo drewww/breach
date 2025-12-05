f76590a701a7e49ff71e62aed09d18660dc5959c
Drew Harry <drew.harry@gmail.com>
2025-12-04 21:05:39 -0800

    Remove unnecessary Leader fields.

--- modules/game/components/leader.lua
+++ modules/game/components/leader.lua
@@ -1,10 +1,8 @@
 --- @class Leader : Component
---- @field mask Bitmask
 local Leader = prism.Component:extend("Leader")
 Leader.name = "Leader"
 
 function Leader:__new()
-   self.followerPositions = { prism.Vector2(-2, 0) }
 end
 
 return Leader
