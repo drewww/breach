87ca254ea366b6a331c6d8645b31fb1f76dcabb2
Drew Harry <drew.harry@gmail.com>
2025-12-31 12:09:02 -0800

    Migrated to a prefab world as a base for the tutorial.

--- modules/game/gamestates/playstate.lua
+++ modules/game/gamestates/playstate.lua
@@ -15,19 +15,12 @@ function PlayState:__new(display, overlayDisplay)
    -- Construct a simple test map using MapBuilder.
    -- In a complete game, you'd likely extract this logic to a separate module
    -- and pass in an existing player object between levels.
-   local builder = prism.LevelBuilder()
-
-   builder:rectangle("line", 0, 0, 32, 32, prism.cells.Wall)
-   -- Fill the interior with floor tiles
-   builder:rectangle("fill", 1, 1, 31, 31, prism.cells.Floor)
-   -- Add a small block of walls within the map
-   builder:rectangle("fill", 5, 5, 7, 7, prism.cells.Wall)
-   -- Add a pit area to the southeast
-   builder:rectangle("fill", 20, 20, 25, 25, prism.cells.Pit)
+   -- local builder = prism.LevelBuilder()
+   local builder = prism.LevelBuilder.fromLz4("modules/game/world/prefab/tutorial/base.lvl")
 
    -- Place the player character at a starting location
    local player = prism.actors.Player()
-   builder:addActor(player, 12, 12)
+   builder:addActor(player, 8, 8)
 
    -- Add systems
    builder:addSystems(prism.systems.SensesSystem(), prism.systems.SightSystem(), prism.systems.ExpiringSystem(),
