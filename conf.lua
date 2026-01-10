edfdbbd38b9bff03a63f104a52aa883a1f727a97
Drew Harry <drew.harry@gmail.com>
2026-01-09 20:23:10 -0800

    Repackaging the web build with a custom html file works.

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@
 function love.conf(t)
    t.window.vsync = 0 -- Enable vsync (1 by default)
    t.window.width = 960
-   t.window.height = 540
+   t.window.height = 650
    t.window.usedpiscale = false
    t.window.title = "breach"
    -- Other configurations...
