49807f5bd9b889b28708de6e20e022e0f3757d9c
Drew Harry <drew.harry@gmail.com>
2026-03-02 10:37:03 -0800

    Cleaned up corners slightly.

--- modules/game/world/tunneler.lua
+++ modules/game/world/tunneler.lua
@@ -7,6 +7,8 @@ local Tunneler = prism.Object:extend("Tunneler")
 
 
 function Tunneler:__new(position, direction)
+   -- width of 1 means 1 on either side, so effective width of 3. we're not going to
+   -- worry about making even-width halls.
    self.width = 1
 
    -- ensure the direction is a unit vector
@@ -22,16 +24,23 @@ end
 function Tunneler:step(builder)
    -- each step, move forward one and dig out a "width" tunnel.
 
-   local perpandicular = self.direction:rotateClockwise()
-
-   local from = self.position - (perpandicular * self.width)
-   local to = self.position + (perpandicular * self.width)
-
-   builder:line(from.x, from.y, to.x, to.y, prism.cells.Floor)
+   self:dig(builder)
 
    -- some chance of rotating
    if RNG:random() < self.turnOdds then
       -- silly but whatever
+      --
+      -- when we rotate, we need to step forward N times to clear out the corner. where N is the width.
+
+      local corner = prism.Vector2(self.position:decompose())
+
+      for i = 1, self.width do
+         self.position = self.position + self.direction
+         self:dig(builder)
+      end
+
+      self.position = corner
+
       self.direction = RNG:random() <= 0.5 and self.direction:rotateClockwise() or
           self.direction:rotateClockwise():rotateClockwise():rotateClockwise()
    end
@@ -39,6 +48,15 @@ function Tunneler:step(builder)
    self.position = self.position + self.direction
 
    return {}
+end
+
+function Tunneler:dig(builder)
+   local perpandicular = self.direction:rotateClockwise()
+
+   local from = self.position - (perpandicular * self.width)
+   local to = self.position + (perpandicular * self.width)
+
+   builder:line(from.x, from.y, to.x, to.y, prism.cells.Floor)
 end
 
 return Tunneler
