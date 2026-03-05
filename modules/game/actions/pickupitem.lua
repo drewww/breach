cd5141359f649b3f5b6ea84405be4a218f45ae7d
Drew Harry <drew.harry@gmail.com>
2026-03-05 15:37:03 -0800

    Auto pickup loot.

--- modules/game/actions/pickupitem.lua
+++ modules/game/actions/pickupitem.lua
@@ -5,7 +5,7 @@ local Item = prism.Target(prism.components.Item):outsideLevel()
 
 PickupItem.targets = { Item }
 
-PickupItem.requiredComponents = { prism.components.Slots }
+PickupItem.requiredComponents = { prism.components.Slots, prism.components.Inventory }
 
 function PickupItem:canPerform()
    return true
@@ -13,6 +13,13 @@ end
 
 function PickupItem:perform(level, item)
    local slots = self.owner:expect(prism.components.Slots)
+   local inventory = self.owner:expect(prism.components.Inventory)
+
+   if (item:getName() == "Credits") then
+      inventory:addItem(item)
+      level:removeActor(item)
+      return
+   end
 
    local slot = slots:insert(item)
 
